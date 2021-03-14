5950d6966e95818bec1d6ba64e2a94fff7228f6e
ahnl <46541386+ahnl@users.noreply.github.com>
2021-03-14 17:07:32 +0200

    Give roles automatically to new guild members

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const config = require('./config.json');
 const discordClient = require('./src/discord.js');
 const httpServer = require('./src/http.js');
 const mailBot = require('./src/mail');  
+
+// Give roles automatically to new guild members
+discordClient.on('guildMemberAdd', member => {
+    member.roles.add(config.discord.autoRole);
+});
