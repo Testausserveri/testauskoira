90e4929a5331b444afa99d8d44e9ee2f78ec137f
ahnl <46541386+ahnl@users.noreply.github.com>
2021-05-02 20:07:56 +0300

    Code structure

--- src/discord.js
+++ src/discord.js
@@ -5,6 +5,16 @@ const discordClient = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REA
 
 discordClient.on('ready', () => {
     console.log(`[DC] Logged in as ${discordClient.user.tag}!`);
+// Give roles automatically to new guild members
+discordClient.on('guildMemberAdd', member => {
+    member.roles.add(config.discord.autoRole);
+});
+discordClient.on('messageReactionAdd', (reaction, user) => {
+    if (reaction.message.id == config.discord.karhu.msgid) {
+        reaction.message.guild.members.fetch(user.id).then(member => {
+            member.roles.add(config.discord.karhu.roleid).catch(console.error);
+        })
+    }
 });
 
 discordClient.on('message' , (msg) => {
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const config = require('../config.json');
+const discordClient = require('./discord.js');
+const httpServer = require('./http.js');
+const mailBot = require('./mail');  
+
+
+
